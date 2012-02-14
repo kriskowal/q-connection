4448eca394d6486ce9b351003a55c2f6e2cd79cc
johnjbarton <johnjbarton@johnjbarton.com>
2012-02-13 16:50:48 -0800

    dead end

--- q-rpc.js
+++ q-rpc.js
@@ -1,23 +1,55 @@
 // Google BSD license http://code.google.com/google_bsd_license.html
 // Copyright 2011 Google Inc. johnjbarton@google.com
 
-/*globals define window*/
+/*globals define exports require window Q_COMM*/
 
-define(['q-comm/q-comm'],
-function(Q_COMM) {
+(function (definition) {
+    var global = this;
+
+    // RequireJS
+    if (typeof define === "function") {
+        define(["q-comm/q-comm"], function (Q_COMM) {
+            var exports = {};
+            var imports = {"Q_COMM": Q_COMM};
+            definition(
+                function (id) {
+                    return imports[id];
+                },
+                exports
+            );
+            return exports;
+        });
+
+    // CommonJS
+    } else if (typeof exports === "object") {
+        definition(require, exports);
+
+    // <script>
+    } else {
+        var imports = {"Q_COMM": Q_COMM};
+        definition(
+            function (id) {
+                return imports[id];
+            },
+            Q_RPC = {}
+        );
+        console.log("Q_RPC", Q_RPC);
+    }
+
+})(function (require, exports) {
 
   // A left paren ( followed by any not-right paren ) followed by right paren
   var reParamList = /\(([^\)]*)\)/; 
   var reParameters = /\(([^\)]*)\)/;
 
 
-function buildPromisingCalls(iface, remote) {
+function buildPromisingCalls(name, iface, remote) {
   var stub = {};
   Object.keys(iface).forEach(function(method) {
     // functions on stub operate on remote
     stub[method] =  function() {
       var args = Array.prototype.slice.call(arguments);       
-      return remote.invoke.apply(remote, [method].concat(args));
+      return remote.invoke.apply(remote, [name+'.'+method].concat(args));
     };
   });
   stub.Q_COMM_CONNECTION = remote;
@@ -46,15 +78,51 @@ function getParameters(handlerSpec) {
 // eventInterface: object with function properties, calls from otherWindow
 // return: object with remote method calls
 
-function makeStub(otherWindow, commands, eventHandlers) {
+function makeStub(otherWindow, commands, local) {
   // build a connection to otherWindow, identifying ourselves as origin
-  var qStub = Q_COMM.Connection(otherWindow, eventHandlers, {origin: window.location.origin});
+  var qStub = Q_COMM.Connection(otherWindow, local, {origin: window.location.origin});
   // wrap the connection in an API, the result object has remote method calls
   return buildPromisingCalls(commands, qStub); 
 }
 
-return {
-  makeStub: makeStub
+exports.makeStub = makeStub;
+
+function register(otherWindow, local, options) {
+
+  var sux = Q_COMM.Connection(otherWindow, local, options);
+  
+  var remote = Object.create(Object.getPrototypeOf(sux));
+  Object.keys(sux).forEach(function(property) {
+    remote[property] = sux[property];
+  });
+  
+  remote.discover = function (remoteName) {
+    return remote.invoke('discover', remoteName).then(
+      function(remoteAPI) {
+        // Remote should give us json specification of function API
+        return buildPromisingCalls(remoteName, remoteAPI, remote);
+      }
+    );
   };
+  
+  local.discover = function(localName) {
+    var props = {};
+    var theLocal = local[localName];
+    if (theLocal) {
+      Object.keys(theLocal).forEach(function(prop) {
+        if (typeof theLocal[prop] === 'function') {
+          props[prop] = true; 
+        }
+      });
+    }
+    return props;
+  };
+
+  return remote;
+}
+
+exports.register = register;
+
+return exports;
   
 });
